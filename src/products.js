52bfcc069d8a947c818595af2748d5aac237bd04
g00415754 <g00415754@atu.ie>
2024-04-10 12:28:56 +0100

    increment button working

--- src/products.js
+++ src/products.js
@@ -1,6 +1,15 @@
-function decrement(){
-    let minus = document.getElementsByClassName("quantity").value;
-    let newMinus = minus - 1;
+function increment(x){
+    //alert(22)
+    if(x==1){
+        // flower garden earrings
+        let incr = Number(document.getElementById("quant").textContent);
+        incr++;
+        document.getElementById("quant").textContent = incr;
+        sessionStorage.product1Qty = incr;
+    }
 
-    document.getElementsByClassName("quantity").write = newMinus;
+    //slet minus = document.getElementsById("quant").value;
+    //let newMinus = minus + 1;
+
+    //document.getElementsById("quant").write = newMinus;
 }
