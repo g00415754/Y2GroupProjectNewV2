f9cea696740b7410423cdb92d3c6067d346ea6bb
Dylan <G00419061@atu.ie>
2024-04-18 19:35:58 +0100

    Revert "go back"
    This reverts commit 1473d930bd8e41a5799a852810a03579d1b785df.

--- JS/checkout.js
+++ JS/checkout.js
@@ -1,19 +1,10 @@
-function validate(){
-    if(checkEmail() && 
-    checkExp() && 
-    checkCvv() && 
-    checkSEmail ()){
-        return true
-    }else{
-        return false
-    }
-}
 const m = new Date();
 let month = m.getMonth()+1;
 const y = new Date();
 let year = y.getFullYear();
+let message = ""
+let invalid = false;
 
-<<<<<<< HEAD
 function Validate(){
     //Billing Validation//
     let name = document.getElementById("nname").value;
@@ -29,27 +20,8 @@ function Validate(){
     if(email.indexOf(".") == -1 || email.indexOf("@") == -1){
         message += "Invalid Email Address for billing\n"
         invalid = true;
-=======
-
-function checkName(){
-    let name = document.getElementById("nname").value;
-    sessionStorage.Name=name;
-}
-document.getElementById("order_con_name").innerHTML = sessionStorage.Name;
-
-function checkEmail(){
-    let x = document.getElementById("email").value;
-    let y = x.value.indexOf("@");
-    let z = x.value.indexOf(".");
-
-    if(y == -1 || z == -1){
-        alert("Invalid Email Address");
-        return false
->>>>>>> parent of baebfcf (updating validation on checkout)
     }else{
-        let email = x.value;
         sessionStorage.Email=email;
-<<<<<<< HEAD
     }
 
     let city = document.getElementById("city").value;
@@ -58,14 +30,9 @@ function checkEmail(){
         invalid = true;
     }else{
         sessionStorage.City=city;
-=======
-        return true
->>>>>>> parent of baebfcf (updating validation on checkout)
     }
     
-}
 
-<<<<<<< HEAD
     let address = document.getElementById("address").value
     if(address.length == 0){
         message += "Please enter Address for billing\n"
@@ -73,41 +40,16 @@ function checkEmail(){
     }else{
         sessionStorage.Address=address;
     }
-=======
-function checkCity(){
-    let city = document.getElementById("city").value;
-    sessionStorage.City=city;
-}
-document.getElementById("order_con_city").innerHTML = sessionStorage.City;
->>>>>>> parent of baebfcf (updating validation on checkout)
 
-function checkAddress(){
-    let address = document.getElementById("address").value;
-    sessionStorage.Address=address;
-}
-document.getElementById("order_con_address").innerHTML = sessionStorage.Address;
-
-function checkZIP(){
     let zip = document.getElementById("zip").value;
-<<<<<<< HEAD
     if(zip.length !=7){
         message += "Invalid ZIP code for billing\n"
         invalid = true;
-=======
-    if(zip.length != 7){
-        alert("Invalid ZIP/EIR code");
-        return false
->>>>>>> parent of baebfcf (updating validation on checkout)
     }else{
         sessionStorage.ZIP=zip;
-        return true;
-    }   
-}
-document.getElementById("order_con_zip").innerHTML = sessionStorage.ZIP;
+    }
 
-function checkCountry(){
     let country = document.getElementById("country").value;
-<<<<<<< HEAD
     if(country.length == 0){
         message += "Please enter Country for billing\n"
         invalid = true;
@@ -156,20 +98,6 @@ function checkCountry(){
     if(cvv.length != 3){
         message += "Invalid CVV number\n"
         invalid = true;
-=======
-    sessionStorage.Country=country;
-}
-document.getElementById("order_con_country").innerHTML = sessionStorage.Country;
-
-function checkSEmail(){
-    let x = document.getElementById("semail");
-    let y = x.value.indexOf("@");
-    let z = x.value.indexOf(".");
-
-    if(y == -1 || z == -1){
-        alert("Invalid Email Address");
-        return false
->>>>>>> parent of baebfcf (updating validation on checkout)
     }else{
     }
 
@@ -240,35 +168,3 @@ function checkSEmail(){
     
 }
 
-<<<<<<< HEAD
-=======
-function checkCvv(){
-    let x = document.getElementById("cvv")
-    let y = x.value.length;
-
-    if(y != 3){
-        alert("INVALID CVV")
-        return false
-    }else{
-        return true
-    }
-}
-
-function checkExp(){
-    let x = document.getElementById("expmonth").value
-    let y = document.getElementById("expyear").value
-    if(y > year){
-        return true
-    }else if(y < year){
-        alert("CARD EXPIRED")
-        return false
-    }else{
-        if(x <= month){
-            alert("CARD EXPIRED")
-            return false
-        }else{
-            return true
-        }
-    }
-}
->>>>>>> parent of baebfcf (updating validation on checkout)
