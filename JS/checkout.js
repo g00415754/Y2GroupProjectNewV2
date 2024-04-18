76efce48c42df1221a6c4141bd72486f137bf1fe
Dylan <G00419061@atu.ie>
2024-04-18 19:42:09 +0100

    Revert "updating validation on checkout"
    This reverts commit baebfcfd766f27b9ef382eac6d018c063bac26c9.

--- JS/checkout.js
+++ JS/checkout.js
@@ -1,108 +1,70 @@
+function validate(){
+    if(checkEmail() && 
+    checkExp() && 
+    checkCvv() && 
+    checkSEmail ()){
+        return true
+    }else{
+        return false
+    }
+}
 const m = new Date();
 let month = m.getMonth()+1;
 const y = new Date();
 let year = y.getFullYear();
-let message = ""
-let invalid = false;
 
-function validate(){
+
+function checkName(){
     let name = document.getElementById("nname").value;
-    if(name == null){
-        message += "Please Enter Your Name\n"
-        invalid = true;
-    }else{
-        sessionStorage.Name=name;
-    }
-    
+    sessionStorage.Name=name;
+}
+document.getElementById("order_con_name").innerHTML = sessionStorage.Name;
 
-    let email = document.getElementById("email").value;
-    if(email.indexOf(".") == -1 || email.indexOf("@") == -1){
-        message += "Invalid Email Address\n"
-        invalid = true;
+function checkEmail(){
+    let x = document.getElementById("email").value;
+    let y = x.value.indexOf("@");
+    let z = x.value.indexOf(".");
+
+    if(y == -1 || z == -1){
+        alert("Invalid Email Address");
+        return false
     }else{
+        let email = x.value;
         sessionStorage.Email=email;
-    }
-
-    let city = document.getElementById("city").value;
-    if(city = ""){
-        message += "Please Enter City\n"
-        invalid = true;
-    }else{
-        sessionStorage.City=city;
+        return true
     }
     
+}
 
-    let address = document.getElementById("address").value
-    if(address = ""){
-        message += "Please Enter Address\n"
-        invalid = true;
-    }else{
-        sessionStorage.Address=address;
-    }
+function checkCity(){
+    let city = document.getElementById("city").value;
+    sessionStorage.City=city;
+}
+document.getElementById("order_con_city").innerHTML = sessionStorage.City;
 
+function checkAddress(){
+    let address = document.getElementById("address").value;
+    sessionStorage.Address=address;
+}
+document.getElementById("order_con_address").innerHTML = sessionStorage.Address;
+
+function checkZIP(){
     let zip = document.getElementById("zip").value;
-    if(zip.length !=7){
-        message += "Invalid ZIP code\n"
-        invalid = true;
+    if(zip.length != 7){
+        alert("Invalid ZIP/EIR code");
+        return false
     }else{
         sessionStorage.ZIP=zip;
-    }
+        return true;
+    }   
+}
+document.getElementById("order_con_zip").innerHTML = sessionStorage.ZIP;
 
+function checkCountry(){
     let country = document.getElementById("country").value;
-    if(country = ""){
-        message += "Please Enter Country\n"
-        invalid = true;
-    }else{
-        sessionStorage.Country=country;
-    }
-
-    let cname = document.getElementById("cname")
-    if(cname = ""){
-        message += "Please Enter Name on your Card\n"
-        invalid = true;
-    }else{
-        sessionStorage.cName=cname;
-    }
-
-    let ccnum = document.getElementById("ccnum")
-    if(ccnum = ""){
-        message += "Please Enter Number on your Card\n"
-        invalid = true;
-    }else if(ccnum.length != 12){
-        message += "Invalid Card Number\n"
-        invalid = true;
-    }else{
-        sessionStorage.ccNum=ccnum;
-    }
-
-    let expmonth = document.getElementById("expmonth").value
-    let expyear = document.getElementById("expyear").value
-    if(expyear > year){
-        sessionStorage.Exp=expmonth;
-    }else if(expyear < year){
-        message += "Card Expired\n"
-        invalid = true;
-    }else{
-        if(expmonth <= month){
-            message += "Card Expired\n"
-            invalid = true;
-        }else{
-            sessionStorage.Exp=expyear;
-        }
-    }
-
-    let cvv = document.getElementById("cvv").value
-    if(cvv.length != 3){
-        message += "Invalid CVV number\n"
-        invalid = true;
-    }else{}
-
+    sessionStorage.Country=country;
 }
-if(invalid){
-    alert("wrong")
-}else{
-}
-    document.getElementById("order_con_name").innerHTML = sessionStorage.Name;
+document.getElementById("order_con_country").innerHTML = sessionStorage.Country;
 
 function checkSEmail(){
     let x = document.getElementById("semail");
@@ -114,5 +76,35 @@ function checkSEmail(){
         return false
     }else{
         return true
+    }
+}
+
+function checkCvv(){
+    let x = document.getElementById("cvv")
+    let y = x.value.length;
+
+    if(y != 3){
+        alert("INVALID CVV")
+        return false
+    }else{
+        return true
+    }
+}
+
+function checkExp(){
+    let x = document.getElementById("expmonth").value
+    let y = document.getElementById("expyear").value
+    if(y > year){
+        return true
+    }else if(y < year){
+        alert("CARD EXPIRED")
+        return false
+    }else{
+        if(x <= month){
+            alert("CARD EXPIRED")
+            return false
+        }else{
+            return true
+        }
     }
 }
